e8d95967fbbc60579d1da12e76852e0485032819
justinmandac <justinmandac@gmail.com>
2017-05-30 21:30:24 +0800

    Implemented semi-functioning import

--- server.js
+++ server.js
@@ -36,23 +36,64 @@ app.get('/events-cached', (req, res) => {
     res.json(parser.parseMsoTable(file));
 });
 
+/**
+ * @param {string} link The link to load data from.
+ * TODO: Promise-fy request and fs.writeFile
+ * TODO: Move link -> filename transformation to its own function.
+*/
+function importLink(link) {
+    const split = link.split('/');
+    const fullFileName = split[split.length - 1].split('.');
+    const fileName = fullFileName[0].split('_');
+    const yearCheck = new RegExp(/\d/);
+
+    fullFileName[1] = 'json'; // change the extension to .json.
+
+    if (!yearCheck.test(fileName[0])) {
+        fullFileName[0] = fileName.reverse().join('_');
+    }
+
+    request(link, (err, response, data) => {
+        if (err) {
+            console.log('ERR! ', err);
+            res.send(err);
+            return;
+        }
+        const jsonFileName = fullFileName.join('.');
+        const parsed = parser.parseMsoTable(data);
+
+        if (parsed.events.length !== 0) {
+            const fileData = JSON.stringify(parsed);
+
+            fs.writeFile(`./json/${jsonFileName}`, fileData, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log(`${jsonFileName} imported successfully.`);
+            });
+        }
+    });
+}
+
 app.get('/import', (req, res) => {
     if (!file) {
         res.send({ err: true});
         return;
     }
+    const { current, legacy } = parser.scrapeLinks(file);
+    console.log('Import started!');
+    current.forEach(l => importLink(l));
 
-    const $ = cheerio.load(file);
-    const links = [];
-    // Get the links to monthly reports.
-    // Some of the links, however, lead to collapsed reports. As of writing,
-    // these reports are of the years 2011- 2014 and cannot be parsed by
-    // parser.parseMsoTable.
-    $('a').filter((i, el) => {
-        return $(el).attr("href").includes("EQLatest-Monthly");
-    }).each((i, el) => {
-        links.push($(el).attr("href"));
-    });
+    res.send({err: null, data: 'import started'});
+});
+
+app.get('/links', (req, res) => {
+    if (!file) {
+        res.send({ err: true});
+        return;
+    }
+    const links = parser.scrapeLinks(file);
     // NOTE collapsed reports' links contain the substring Jan-Dec
 
     res.send({err : null, data: links});
